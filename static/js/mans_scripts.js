392e271d9cd95564710a827cd42a8a0c85373913
Markuss <markusspriede@inbox.lv>
2021-03-02 14:13:39 +0200

    Pievienots JS

---
+++ static/js/mans_scripts.js
@@ -0,0 +1,8 @@
+console.log("viss strādā!")
+//alert("Hello!")
+alert("Markuss!")
+
+let mainigaisTeksts = "skola"
+let mainigaisSkaitlis = 7
+console.log(mainigaisTeksts)
+console.log(mainigaisSkaitlis) 
